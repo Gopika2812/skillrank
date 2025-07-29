1a99937f4f92baed81897259f12d36e4030cb47a
Gopika2812 <gopika.p2812@gmail.com>
2025-07-29 22:18:00 +0530

    backend changes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
+import os
 
 
 app = Flask(__name__)
@@ -12,7 +13,7 @@ client = MongoClient(MONGO_URI)
 db = client["skillrank"]
 users_collection = db["users"]
 
-# Ensure email is unique
+
 users_collection.create_index("email", unique=True)
 
 @app.route("/signUp", methods=["POST"])
@@ -26,10 +27,9 @@ def sign_up():
     try:
         users_collection.insert_one({"email": email})
         return jsonify({"message": "Signup successful!"}), 201
-    except DuplicateKeyError:
-        return jsonify({"error": "Email already exists"}), 409
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
